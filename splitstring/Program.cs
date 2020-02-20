5acea1252894cb410196cc61e3b1dba5505171e9
15341a1205 <41323544+15341a1205@users.noreply.github.com>
2020-02-20 15:38:59 +0530

    Initial commit

---
+++ splitstring/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace splitstring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            fullname("Supraja Baratam");
+        }
+        static void fullname(string arr)
+        {
+            string[] s = arr.Split();
+            foreach (string p in s)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
